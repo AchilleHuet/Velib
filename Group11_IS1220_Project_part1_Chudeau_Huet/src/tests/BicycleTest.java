0dc83abe28ce80e42b72937aea7f48b02112cdcd
DamienChudeau <37755035+DamienChudeau@users.noreply.github.com>
2018-03-28 18:13:42 +0200

    18h13

--- Group11_IS1220_Project_part1_Chudeau_Huet/src/tests/BicycleTest.java
+++ Group11_IS1220_Project_part1_Chudeau_Huet/src/tests/BicycleTest.java
@@ -11,19 +11,16 @@ public class BicycleTest {
 
 	@Test
 	public void testBicycle() {
-<<<<<<< HEAD
-		Bicycle bike_meca = new Bicyle(BicyleType(Mechanical));
-		System.out.println(bike_meca.type() + bike_meca.ID());
-		assertTrue(bike_meca.type == BicyleType.Mechanical);
-=======
+		
 		Bicycle bike_meca = new Bicycle(BicycleType.Mechanical);
-		System.out.println(bike_meca.getType() + bike_meca.getID());
-		assertTrue(bike_meca.getType() == BicycleType.Mechanical);
->>>>>>> 50029abf01153c593023a3d6f39856c4c7c2271c
+		System.out.println(bike_meca.getType());
+		System.out.println(bike_meca.getID());
+		assertTrue(bike_meca.type == BicycleType.Mechanical);
 		
 		Bicycle bike_elec = new Bicycle(BicycleType.Electrical);
-		System.out.println(bike_elec.getType() + bike_elec.getID());
-		assertTrue(bike_meca.getType() == BicycleType.Electrical);
+		System.out.println(bike_elec.getType());
+		System.out.println(bike_elec.getID());
+		assertTrue(bike_elec.type == BicycleType.Electrical);
 	}
 
 }
